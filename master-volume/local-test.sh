05e4ad3651475afd5df05061a2bde510ebc8053f
Niv <niv.cyber@gmail.com>
2022-08-24 11:43:24 +0300

    added basic playbook functionality

---
+++ master-volume/local-test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo This is a test script from the local machine
+
+cd /root
+
+mkdir -p hello
+
---
+++ master-volume/node-test.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo This text file was created by the node-test script executed in the node machine > hello.txt
